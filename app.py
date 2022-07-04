6ca9fe24e2de2fb2b0755193014f45799e598e44
abdallahayman77 <83986510+abdallahayman77@users.noreply.github.com>
2022-07-04 19:56:27 +0200

    Add existing file

--- app.py
+++ app.py
@@ -260,12 +260,3 @@ def success():
 
 if __name__ == "__main__":
     app.run(debug = True)
-    
-def create_app(config_file='settings.py'):
-    app = Flask(__name__)
-
-    app.config.from_pyfile(config_file)
-
-    db.init_app(app)
-
-    return app
